3fe594a734b762bbf26db566cf8b6bdc4e9bcea4
Rodlikespants <rodwittlai@gmail.com>
2020-04-26 22:12:25 -0400

    Refactored and fixed solution

--- solutions/7/7b.rb
+++ solutions/7/7b.rb
@@ -1,5 +1,82 @@
 require 'pry'
 
+class Amplifier
+	attr_reader :intcode, :phase, :terminated, :pos, :input_prompts, :output
+
+	def initialize(og_inputs, phase)
+		@intcode = og_inputs.clone
+		@phase = phase
+		@terminated = false
+		@pos = 0
+		@input_prompts = [phase]
+		@output = 0
+	end
+
+	def process_operation(full_code)
+		opcode = full_code%100
+		mode1 = (full_code/100)%10
+		mode2 = (full_code/1000)%10
+		mode3 = (full_code/10000)
+		# puts "#{full_code}"
+
+		num_positions = 0
+		if (opcode == 1)
+			return Addition.new(mode1, mode2, mode3)
+		elsif (opcode == 2)
+			return Multiplication.new(mode1, mode2, mode3)
+		elsif (opcode == 3)
+			return Input.new(mode1, mode2, mode3, @input_prompts.pop)
+		elsif (opcode == 4)
+			return Output.new(mode1, mode2, mode3)
+		elsif (opcode == 5)
+			return JumpIfTrue.new(mode1, mode2, mode3)
+		elsif (opcode == 6)
+			return JumpIfFalse.new(mode1, mode2, mode3)
+		elsif (opcode == 7)
+			return LessThan.new(mode1, mode2, mode3)
+		elsif (opcode == 8)
+			return Equals.new(mode1, mode2, mode3)
+		elsif (opcode == 99)
+			return Exit.new(mode1, mode2, mode3)
+		else
+			raise "Unknown operation full code #{full_code}"
+		end
+	end	
+
+	def compute(thruster_input=nil)
+		if (!thruster_input.nil?)
+			@input_prompts.insert(0, thruster_input)
+		end
+
+		puts "input_prompts=" + @input_prompts.to_s
+
+		op = self.process_operation(@intcode[@pos])
+		taking_input = false
+
+		while (!@terminated && @pos < @intcode.size && !taking_input)
+			p @intcode[@pos..(@pos+op.instruction_offset-1)]
+
+			result = op.process(@intcode, @pos)
+			if (op.opcode == 3 && result.nil?)
+				taking_input = true
+				next
+			end
+
+			@intcode = result.intcode
+
+			# hardcoded for this level?
+			if (!result.output.nil?)
+				@output = result.output.to_i
+				puts "output changed to #{output}"
+			end
+
+			@pos += op.instruction_offset
+			op = self.process_operation(@intcode[@pos])
+			@terminated = op.opcode == 99
+		end
+	end
+end
+
 class OperationResult
 	attr_reader :intcode, :output
 	def initialize(intcode, output=nil)
@@ -70,7 +147,9 @@ class Input < Operation
 	def process(inputs, pos)
 		# input_value = "5".chomp.to_i
 		if (@input_prompt.nil?)
-			input_value = $stdin.gets.chomp.to_i
+			# input_value = $stdin.gets.chomp.to_i
+			# move onto next amplifier if no inputs
+			return nil
 		else
 			puts "Using input prompt " + @input_prompt.to_s
 			input_value = @input_prompt
@@ -177,146 +256,42 @@ class Exit < Operation
 	end
 end
 
-def process_operation(full_code, input_prompt=nil)
-	opcode = full_code%100
-	mode1 = (full_code/100)%10
-	mode2 = (full_code/1000)%10
-	mode3 = (full_code/10000)
-	# puts "#{full_code}"
-
-	num_positions = 0
-	if (opcode == 1)
-		return Addition.new(mode1, mode2, mode3)
-	elsif (opcode == 2)
-		return Multiplication.new(mode1, mode2, mode3)
-	elsif (opcode == 3)
-		return Input.new(mode1, mode2, mode3, input_prompt)
-	elsif (opcode == 4)
-		return Output.new(mode1, mode2, mode3)
-	elsif (opcode == 5)
-		return JumpIfTrue.new(mode1, mode2, mode3)
-	elsif (opcode == 6)
-		return JumpIfFalse.new(mode1, mode2, mode3)
-	elsif (opcode == 7)
-		return LessThan.new(mode1, mode2, mode3)
-	elsif (opcode == 8)
-		return Equals.new(mode1, mode2, mode3)
-	elsif (opcode == 99)
-		return Exit.new(mode1, mode2, mode3)
-	else
-		raise "Unknown operation full code #{full_code}"
-	end
-end
-
-def compute_all(og_inputs, result)
-	for i in 0..99
-		for j in 0..99
-			computed_result = compute(og_inputs,i,j)
-			if (result == computed_result)
-				return i, j
-			end
-		end
-	end
-end
-
-def compute(og_inputs, noun=nil, verb=nil, phase=nil, thruster_input=nil)
-	inputs = og_inputs.clone
-	# p inputs
-	if (!noun.nil?)
-		inputs[1] = noun
-	end
-
-	if (!verb.nil?)
-		inputs[2] = verb
-	end
-
-	pos = 0
-
-	input_prompt = phase
-	op = process_operation(inputs[pos], input_prompt)
-	num_input_prompts = 0
-	output = nil
-
-	if (op.opcode == 3)
-		num_input_prompts += 1
-	end
-
-	while (op.opcode != 99 && pos < inputs.size)
-		p inputs[pos..(pos+op.instruction_offset-1)]
-
-		# ugly hardcoded logic here
-		if (op.opcode == 3)
-			if (num_input_prompts > 0)
-				input_prompt = thruster_input
-			end
-			num_input_prompts += 1
-		end
-
-		result = op.process(inputs, pos)
-		inputs = result.intcode
-
-		# hardcoded for this level?
-		if (!result.output.nil?)
-			output = result.output
-		end
-
-		pos += op.instruction_offset
-		op = process_operation(inputs[pos], input_prompt)
-	end
-
-	puts output
-	puts op.opcode
-	return inputs[0], output.to_i, op.opcode == 99
-end
-
-def simulate_thrusters(intcode)
-	max_output = 0
-	max_perm = nil
-	phase_perms = [0,1,2,3,4].permutation.to_a
-	phase_perms.each do | phases |
-		thruster_code = 0
-		puts "phase_perm: #{phases}"
-		phases.each do | phase |
-			thruster_inputs = intcode.clone
-			last_result, thruster_code = compute(thruster_inputs, nil, nil, phase, thruster_code)
-			puts "new thruster code=#{thruster_code}"
-			if (thruster_code > max_output)
-				max_output = thruster_code
-				max_perm = phases
-			end
-		end
-		puts "Max output: #{max_output}"
-	end
-	# binding.pry
-	return max_output, max_perm
-end
-
 # like the previous method except again with new phase settings
 def simulate_thruster_feedback(intcode)
 	max_output = 0
 	max_perm = nil
-	count = 0
-	# phase_perms = [5,6,7,8,9].permutation.to_a
-	phase_perms = [[9,8,7,6,5]]
+	iterations = 0
+	phase_perms = [5,6,7,8,9].permutation.to_a
+	# phase_perms = [[9,8,7,6,5]]
+
 	phase_perms.each do | phases |
 		# thruster_code = input
 		thruster_code = 0
+		
+		# initialize amplifiers to the phases
+		amps = Array.new
+		phases.each do | phase |
+			amps.push(Amplifier.new(intcode, phase))
+		end
+
 
 		puts "phase_perm: #{phases}"
-		cycle = phases.cycle
-		phase = cycle.next
-		exited = false
-		while (count < 25)
-			puts "phase=#{phase}"
-			thruster_inputs = intcode.clone
-			last_result, thruster_code, exited = compute(thruster_inputs, nil, nil, phase, thruster_code)
-			puts "new thruster code=#{thruster_code} on phase=#{phase}"
+		cycle = amps.cycle
+		amp = cycle.next
+		while (!amp.terminated || !amp.eql?(amps.last))
+			puts "\n[phase=" + amp.phase.to_s + "]"
+
+			amp.compute(thruster_code)
+			thruster_code = amp.output
+			puts "new thruster code=#{thruster_code} on phase="+amp.phase.to_s
+
 			if (thruster_code > max_output)
 				max_output = thruster_code
 				max_perm = phases
 			end
-			phase = cycle.next
-			count += 1
+
+			amp = cycle.next
+			iterations += 1
 		end
 		puts "Max feedback output: #{max_output}"
 	end
@@ -339,7 +314,6 @@ end
 # binding.pry
 
 # output, phase = simulate_thrusters(intputs)
-puts "FIRST PHASE COMPLETE\n\n\n"
 feedback_output, feedback_phase = simulate_thruster_feedback(intputs)
 
 # puts "Max output: #{output}"
